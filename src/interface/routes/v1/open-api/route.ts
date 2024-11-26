5d782d4574e2087abc09b262e50b9b78256564bb
Derek McIntire <derekbmcintire@gmail.com>
2024-11-26 10:00:09 -0500

    Fix comment

--- src/interface/routes/v1/open-api/route.ts
+++ src/interface/routes/v1/open-api/route.ts
@@ -3,8 +3,8 @@ import { FastifyInstance } from 'fastify';
 const API_SCHEMA_URL_PATH = `/openapi.json`;
 
 /**
- * basic health check route at the root ("/").
- * This route is often used for monitoring the server's status.
+ * Add an endpoint to serve the OpenAPI spec as a JSON file.
+ * This allows you to access the OpenAPI spec at `/openapi.json`.
  */
 export default async function openApiSchemaRoute(fastify: FastifyInstance) {
   fastify.get(API_SCHEMA_URL_PATH, (_request, reply) => {
